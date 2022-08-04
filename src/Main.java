6929db37e3fa6c5f81f659cc40132dca97f2f907
Inna <inna-asbest@mail.ru>
2022-08-05 00:14:00 +0500

    первый коммит

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Привет, мир!");
+
+
+        //Задание 1
+
+        int clientOS = 0;
+        if (clientOS >= 1) {
+            System.out.println("Установите версию для iOS по ссылке");
+        } else {
+            System.out.println("Установите версию для Android по ссылке");
+        }
     }
 
 }
