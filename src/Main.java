8ce25a6ea918bcf42324a9e96daf0ffd25e0215a
Inna <inna-asbest@mail.ru>
2022-08-05 01:48:10 +0500

    первый коммит

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,37 @@ public class Main {
 
         //Задание 1
 
-        int clientOS = 0;
-        if (clientOS >= 1) {
+        int clientIOS = 0;
+        if (clientIOS >= 1) {
             System.out.println("Установите версию для iOS по ссылке");
         } else {
             System.out.println("Установите версию для Android по ссылке");
+
+
+                            //Задание 2
+
+                int clientAndroid1 = 0;
+                int PhonAge1 = 2015;
+                if (clientAndroid1 == 1 && PhonAge1 <= 2014) {
+                    System.out.println("Установите облегченную версию для Андроид по ссылке");
+                    if (clientAndroid1 == 1 && PhonAge1 >= 2015) {
+                        System.out.println("Установите версию для Андроид по ссылке");
+                    } else
+                        if (clientAndroid1 == 0 && PhonAge1 >= 2015) {
+                            System.out.println("Установите версию для iOS по ссылке");
+                        } else {
+                            System.out.println("Установите облегченную версию для иос по ссылке");
+
+                    }
+                }
+
+
+
+
+            }
+
         }
     }
+
 
 }
